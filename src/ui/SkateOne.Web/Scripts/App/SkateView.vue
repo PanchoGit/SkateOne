52a9411551b1e181327791b723fdce527516c13f
Francisco <francisco.rosales@gmail.com>
2016-06-08 17:28:08 -0300

    Vue implementation

---
+++ src/ui/SkateOne.Web/Scripts/App/SkateView.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="button" style="position: absolute; margin: auto; top: 30px; left: 0px; right: 0px;" v-on:click="goHomeClick">Home</button>
+    <input name="userName" style="position: absolute; top: 80px; left: 50%; width: 400px; margin-left: -225px" />
+    <div id="grid1Div" style="width:920px;height:320px;overflow:auto;background-color:#DEDEDE; top: 150px; left: 50%; margin-left: -460px; position:absolute"></div>
+</template>
+
+<script>
+    <!-- Component code goes here. -->
+</script>
+
+<style>
+.my-component h2 {
+	color:red;
+}
+</style>
