a7e1f9cdf82dbeb31b462f25ecc9f21510a596ae
Pancho <francisco.rosales@gmail.com>
2017-07-23 16:55:27 -0300

    Updated

--- tools/EncryptConfig.bat
+++ tools/EncryptConfig.bat
@@ -1,2 +1,2 @@
 
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Aspnet_regiis.exe" -pef "connectionStrings" "C:\Projects\SkateOne\src\SkateOne.Web"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Aspnet_regiis.exe" -pef "connectionStrings" "D:\Projects\SkateOne\src\SkateOne.Web"
